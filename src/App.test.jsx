9d495906b2588cb615260752cab75e9d1ba9f797
Yuval <yuvaldar@gmail.com>
2026-05-25 22:43:08 +0300

    fix fail testing - to preactice at GH-actions

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -17,7 +17,7 @@ describe('App', () => {
     expect(screen.getByRole('button', { name: /count is 1/i })).toBeInTheDocument()
   })
 
-  it('always fails', () => {
-    expect(true).toBe(false)
+  it('always true', () => {
+    expect(true).toBe(true)
   })
 })
